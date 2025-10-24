6abed0711520269db7478ceeda838af8a950d0a4
Joshthefullstack <ibiamjoshua20@gmail.com>
2025-10-24 11:20:15 +0100

    feat: allow eoecific origins with credentials

--- birthday-wishlist-server/src/index.ts
+++ birthday-wishlist-server/src/index.ts
@@ -1,18 +1,18 @@
-import express, { NextFunction, Request, Response } from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import compression from 'compression';
-import dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { NextFunction, Request, Response } from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
 
-import router from '../routers/index'
+import router from "../routers/index";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI || '';
+const MONGO_URI = process.env.MONGO_URI || "";
 
 const allowedOrigins = [
   // "https://your-frontend-name.onrender.com", // your deployed frontend URL
@@ -20,46 +20,45 @@ const allowedOrigins = [
   "http://localhost:3001",
 ];
 
+const origins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://wishlist-application-frontend.onrender.com",
+];
 
 // Middleware
 app.use(helmet());
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "http://localhost:3001",
-//       "https://wishlist-application-frontend.onrender.com",
-//     ],
-//     credentials: true,
-//   })
-// );
-app.use(cors({
-  origin: '*'
-}))
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORs"));
+      }
+    },
+    credentials: true
+  }),
+);
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(compression());
 
-app.get('/', (req, res) => {
-  res.send('Birthday Wishlist API is running');
+app.get("/", (req, res) => {
+  res.send("Birthday Wishlist API is running");
 });
 
-
-app.use('/', router());
-
-
+app.use("/", router());
 
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
-  console.log(err)
+  console.log(err);
   res.status(500).json({
-    error: 'Internal server error',
-    message: 'An error occured on the server'
-  })
-})
-
-
+    error: "Internal server error",
+    message: "An error occured on the server",
+  });
+});
 
 // 404 error handler
 
@@ -67,11 +66,10 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   // mongoose.Promise = Promise;
   mongoose.connect(MONGO_URI);
-  mongoose.connection.on('error', (err) => {
+  mongoose.connection.on("error", (err) => {
     console.error(`MongoDB connection error: ${err}`);
     process.exit(1);
   });
 });
 
 export default app;
-
